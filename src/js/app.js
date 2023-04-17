f09fc9c2bcb7c3552aa6d7a21c8dfda5d29dfec2
Tara <tmarieshelnutt@gmail.com>
2023-04-17 04:33:35 +0000

    Firstiesgit add .! 4/16

--- src/js/app.js
+++ src/js/app.js
@@ -27,15 +27,31 @@ function render(variables = {}) {
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  if ((JSON.parse(variables.includeCover == false)) cover = "<div class='cover'></div>";
+
+  if (variables.name == null) variables.name = "Name";
+  if (variables.lastname == null) variables.lastname = "Last Name";
+
+  if (variables.socialMediaPosition == null) variables.socialMediaPosition = "picker";
+
+  if (varibales.role == null) variables.role = "Role";
+
+  if (varibales.city == null) variables.city = "City";
+  if (varibales.country == null) variables.country = "Country";
+
+  if (variables.twitter != null) variables.twitter = "Twitter";
+  if (variables.github != null) variables.github = "Github";
+  if (variables.instagram != null) variables.instagram = "Instagram";
+
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>"${variables.name}" "${variables.lastname}"</h1>
+          <ul class="${varibles.socialMediaPosition}">
+          <h2>"${varibales.role}"<h2>
+          <h3>"${varibles.city}", "${varibles.country}"</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
